2cfe3f3999c238208e36576af39859acff9941d8
Yurii Korokin <yurii.korokin@gmail.com>
2020-02-15 21:17:45 +0200

    Add ability to track youtube channels for new videos.

--- TelegramBotHost/TelegramBotHost.Android/BotServer.cs
+++ TelegramBotHost/TelegramBotHost.Android/BotServer.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
 using Android.Content;
-using Google.Apis.Services;
-using Google.Apis.YouTube.v3;
+using Newtonsoft.Json;
 using Telegram.Bot;
 using Telegram.Bot.Types;
 using Xamarin.Essentials;
@@ -12,11 +15,16 @@ namespace TelegramBotHost.Droid
     [BroadcastReceiver]
     public class BotServer : BroadcastReceiver
     {
+        // GET https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UC0fW0JbGMFvqYOY3V6p-KRg&maxResults=1&order=date&type=video&key=AIzaSyBAqAfCmWExFtq8YRVRKJjHI03IKTAvyGU
+
         private const string OffsetString = "offset";
+        private const string ChannelsString = "channels";
 
         private const string Token = "782581289:AAG3Ti-BqmFol4fksF-YBeQtz0fcQ_5yJ0o";   // @EzhiSarmatMargo_Bot
         private const string YouTubeApiKey = "AIzaSyBAqAfCmWExFtq8YRVRKJjHI03IKTAvyGU";
         private static TelegramBotClient bot;
+
+        private static long chatId = 0;
 
         static BotServer()
         {
@@ -24,6 +32,18 @@ namespace TelegramBotHost.Droid
         }
 
         public override void OnReceive(Context context, Intent intent)
+        {
+            try
+            {
+                HandleTelegramBot();
+            }
+            catch (Exception ex)
+            {
+                bot.SendTextMessageAsync(chatId, ex.ToString());
+            }
+        }
+
+        private static void HandleTelegramBot()
         {
             var offset = Preferences.Get(OffsetString, 0);
 
@@ -56,11 +76,16 @@ namespace TelegramBotHost.Droid
                 offset = update.Id + 1;
                 Preferences.Set(OffsetString, offset);
             }
+
+            CheckChannelsForUpdates();
         }
 
         private static void HandleMessage(Message message)
         {
-            switch (message.Text.ToLower())
+            chatId = message.Chat.Id;
+
+            var words = message.Text.Split(' ');
+            switch (words[0].ToLower())
             {
                 case "hi":
                     bot.SendTextMessageAsync(message.Chat.Id, "Hello", replyToMessageId: message.MessageId);
@@ -72,10 +97,37 @@ namespace TelegramBotHost.Droid
                     HandleYouTubeQuery(message);
                     break;
                 case "help":
-                    bot.SendTextMessageAsync(message.Chat.Id, "Пиши 'ежи'.", replyToMessageId: message.MessageId);
+                    bot.SendTextMessageAsync(
+                        message.Chat.Id,
+                        "'ежи' - видосы с Маргинальных Хайлайтов за последние сутки.\n" +
+                        "'add <youTubeChannekId>' - добавить канал в список отслеживаемых.\n" +
+                        "'remove <youTubeChannekId>' - удалить канал из списка отслеживаемых.\n" +
+                        "'channels' - показать список отслеживаемых каналов.",
+                        replyToMessageId: message.MessageId);
+                    break;
+                case "add":
+                    AddChannel(words[1], message.Chat.Id);
+                    break;
+                case "remove":
+                    RemoveChannel(words[1], message.Chat.Id);
+                    break;
+                case "channels":
+                    var jsonChannels = Preferences.Get(ChannelsString, "");
+                    var trackedChannels = JsonConvert.DeserializeObject<List<TrackedChannelModel>>(jsonChannels);
+
+                    var idList = trackedChannels?.Where(ch => ch.TelegramChatId == message.Chat.Id)
+                        .Select(ch => ch.ChannelId).ToList();
+
+                    if (trackedChannels == null || trackedChannels.Count == 0 || idList == null || idList.Count == 0)
+                    {
+                        bot.SendTextMessageAsync(message.Chat.Id, "List of channels is empty");
+                        break;
+                    }
+
+                    bot.SendTextMessageAsync(message.Chat.Id, string.Join(",\n", idList), replyToMessageId: message.MessageId);
                     break;
                 default:
-                    bot.SendTextMessageAsync(message.Chat.Id, "Message: " + message.Text + "\nWrite \"help\"", replyToMessageId: message.MessageId);
+                    bot.SendTextMessageAsync(message.Chat.Id, "\nWrite \"help\"", replyToMessageId: message.MessageId);
                     break;
             }
         }
@@ -83,22 +135,100 @@ namespace TelegramBotHost.Droid
         // Gets last day videos from YouTube Channel
         private static void HandleYouTubeQuery(Message message)
         {
-            YouTubeService yt = new YouTubeService(new BaseClientService.Initializer() { ApiKey = YouTubeApiKey });
+            bot.SendTextMessageAsync(message.Chat.Id, "TBD", replyToMessageId: message.MessageId);
+        }
 
-            var searchListRequest = yt.Search.List("snippet");
-            searchListRequest.ChannelId = "UC0fW0JbGMFvqYOY3V6p-KRg";   // "Маргинальные Хайлайты"
-            searchListRequest.PublishedAfter = DateTime.Now.AddDays(-1);
-            var searchListResult = searchListRequest.Execute();
+        private static void CheckChannelsForUpdates()
+        {
+            var jsonChannels = Preferences.Get(ChannelsString, "");
+            var trackedChannels = JsonConvert.DeserializeObject<List<TrackedChannelModel>>(jsonChannels);
 
-            var retString = "";
-
-            foreach (var item in searchListResult.Items)
+            if (trackedChannels == null)
             {
-                retString += "\n\nName: " + item.Snippet.Title.Replace("&quot;", "\"") + "\n" + "Url: https://www.youtube.com/watch?v=" + item.Id.VideoId;
+                trackedChannels = new List<TrackedChannelModel>();
             }
 
+            foreach (var channel in trackedChannels)
+            {
+                WebRequest request = WebRequest.Create($"https://www.youtube.com/channel/{channel.ChannelId}/videos");
+                WebResponse response = request.GetResponse();
+                using (Stream stream = response.GetResponseStream())
+                {
+                    using (StreamReader reader = new StreamReader(stream))
+                    {
+                        var html = reader.ReadToEnd();
+                        var videoId = html.Substring(html.IndexOf("/watch?v="), 20);
 
-            bot.SendTextMessageAsync(message.Chat.Id, retString, replyToMessageId: message.MessageId);
+                        if (channel.LastVideoId != videoId)
+                        {
+                            bot.SendTextMessageAsync(channel.TelegramChatId, $"https://www.youtube.com/{videoId}");
+                            channel.LastVideoId = videoId;
+                        }
+                    }
+                }
+            }
+
+            jsonChannels = JsonConvert.SerializeObject(trackedChannels);
+            Preferences.Set(ChannelsString, jsonChannels);
+        }
+
+        private static void AddChannel(string channelId, long chatId)
+        {
+            var jsonChannels = Preferences.Get(ChannelsString, "");
+            var trackedChannels = JsonConvert.DeserializeObject<List<TrackedChannelModel>>(jsonChannels);
+
+            if (trackedChannels == null)
+            {
+                trackedChannels = new List<TrackedChannelModel>();
+            }
+
+            var isAlreadyTracked = trackedChannels.Any(ch => ch.ChannelId == channelId && ch.TelegramChatId == chatId);
+
+            if (!isAlreadyTracked)
+            {
+                trackedChannels.Add(
+                   new TrackedChannelModel
+                   {
+                       ChannelId = channelId,
+                       LastVideoId = string.Empty,
+                       TelegramChatId = chatId
+                   });
+
+                bot.SendTextMessageAsync(chatId, channelId + " was successfully added to channel list.");
+            }
+            else
+            {
+                bot.SendTextMessageAsync(chatId, channelId + " is already exists in the channel list.");
+            }
+
+            jsonChannels = JsonConvert.SerializeObject(trackedChannels);
+            Preferences.Set(ChannelsString, jsonChannels);
+        }
+
+        private static void RemoveChannel(string channelId, long chatId)
+        {
+            var jsonChannels = Preferences.Get(ChannelsString, "");
+            var trackedChannels = JsonConvert.DeserializeObject<List<TrackedChannelModel>>(jsonChannels);
+
+            if (trackedChannels == null)
+            {
+                trackedChannels = new List<TrackedChannelModel>();
+            }
+
+            var isRemoved = trackedChannels.Remove(
+                trackedChannels.FirstOrDefault(ch => ch.ChannelId == channelId && ch.TelegramChatId == chatId));
+
+            if (isRemoved)
+            {
+                bot.SendTextMessageAsync(chatId, channelId + " was successfully removed from channel list.");
+            }
+            else
+            {
+                bot.SendTextMessageAsync(chatId, channelId + " wasn't found in channel list.\nPlease check the channel id spelling.");
+            }
+
+            jsonChannels = JsonConvert.SerializeObject(trackedChannels);
+            Preferences.Set(ChannelsString, jsonChannels);
         }
     }
 }
---
+++ TelegramBotHost/TelegramBotHost.Android/TrackedChannelModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace TelegramBotHost.Droid
+{
+    public class TrackedChannelModel
+    {
+        public string ChannelId { get; set; }
+
+        //public DateTime LastCheck { get; set; }
+        public string LastVideoId { get; set; }
+
+        public long TelegramChatId { get; set; }
+    }
+}
